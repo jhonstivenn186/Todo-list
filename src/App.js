0870c5766ca4c3697bf24f7f6603a743efca4e65
jhonstivenn186 <hashiram123@gmail.com>
2022-12-13 21:21:01 -0500

    tercer  commit

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1 className="t-stroke-shadow">TodoMatic</h1>
       <Form addTask={addTask} />
 
       <div className="filters btn-group stack-exception">
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 
 const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
+  { id: "todo-0", name: "Comer", completed: true },
+  { id: "todo-1", name: "Dormir", completed: false },
+  { id: "todo-2", name: "Hacer ejercicio", completed: false }
 ];
 
 
